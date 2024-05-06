15f4b48d0d8e4ebc2b1399994c23580ed7dbce6c
MatviiTorop <ipz224_tmd@student.ztu.edu.ua>
2024-05-06 22:53:10 +0300

    Created feature of saving sudoku state

---
+++ Classes/Memento/SudokuSnapshot.cs
@@ -0,0 +1,26 @@
+﻿using Classes.SudokuTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Memento
+{
+    public class SudokuSnapshot : ISnapshot
+    {
+        public int[,] SudokuState { get; }
+        
+        public DateTime creationDate { get; } = DateTime.Now;
+        
+        public SudokuSnapshot(int[,] sudokuState)
+        {
+            SudokuState = (int[,])sudokuState.Clone();
+        }
+        
+        public int[,] GetState()
+        {
+            return SudokuState;
+        }
+    }
+}
